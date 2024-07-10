31220d4fe5d604ef2166fb27953aa5639cf68d7a
Arun-Gopalakrishnan <gavk.1986@gmail.com>
2024-07-10 13:56:08 +0530

    Update streamlit_utils.py

--- streamlit_utils.py
+++ streamlit_utils.py
@@ -7,26 +7,16 @@ load_dotenv()
 EXTRACT_TASK_ENTITIES_URI = 'https://ec-task-automation.streamlit.app/extract_task_entities'
 EXTRACT_TEXT_FROM_AUDIO_URI = 'https://ec-task-automation.streamlit.app/extract_text_from_audio'
 
-def get_fileInfo(file):
-    return {"audio_file": (file.name, file, 'multipart/form-data')}
-
-def get_api_response(URI, file):
-    response = response = requests.post(
-        URI,
-        files = get_fileInfo(file)
-    )
-    return response
-
-def get_task_entities(file):
+def get_api_response(file):
     response = response = requests.post(
         EXTRACT_TASK_ENTITIES_URI,
-        files = {"audio_file": (file.name, file, 'multipart/form-data')}
+        files={"audio_file": (file.name, file, 'multipart/form-data')}
     )
     return response.json()
 
-def get_text_from_audio(file):
+def get_api_response_as_text(file):
     response = response = requests.post(
         EXTRACT_TEXT_FROM_AUDIO_URI,
-        files = {"audio_file": (file.name, file, 'multipart/form-data')}
+        files={"audio_file": (file.name, file, 'multipart/form-data')}
     )
-    return response.content
+    return response.text
