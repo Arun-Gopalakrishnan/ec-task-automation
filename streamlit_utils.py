da8a61d162f22265a0b99638fd0ee95c96259a93
Arun-Gopalakrishnan <gavk.1986@gmail.com>
2024-07-10 14:22:59 +0530

    Update streamlit_utils.py

--- streamlit_utils.py
+++ streamlit_utils.py
@@ -4,12 +4,11 @@ import os
 
 load_dotenv()
 
-EXTRACT_TASK_ENTITIES_URI = 'https://ec-task-automation.streamlit.app/extract_task_entities'
-EXTRACT_TEXT_FROM_AUDIO_URI = 'https://ec-task-automation.streamlit.app/extract_text_from_audio'
+EXTRACT_URI = 'https://asr-task-automation-ec.onrender.com/extract_task_entities'
 
 def get_api_response(file):
     response = response = requests.post(
-        EXTRACT_TASK_ENTITIES_URI,
+        EXTRACT_URI,
         files={"audio_file": (file.name, file, 'multipart/form-data')}
     )
     return response.json()
