91173640b6ce0a51a6498eee21aa5fd2b4dc3a6a
Arun-Gopalakrishnan <gavk.1986@gmail.com>
2024-07-10 13:20:22 +0530

    Update api.py

--- api.py
+++ api.py
@@ -15,8 +15,6 @@ app = FastAPI()
 def read_root():
     return {"Status": "Working"}
 
-# query: Hey, I just completed my api development task, which is of high priority. I tool 2 days to complete it.
-
 @app.post("/extract_task_entities", response_model=TaskDetails)
 async def extract_task_entities(
     audio_file: UploadFile = File(...)
@@ -33,3 +31,19 @@ async def extract_task_entities(
 
     entities = extract_details(text)
     return entities
+
+@app.post("/extract_text_from_audio", response_model=str)
+async def extract_text_from_audio(
+    audio_file: UploadFile = File(...)
+    ):
+    # save the audio file
+    file_extension = audio_file.filename.split(".")[-1]
+    tempfile = f"res/audio_file.{file_extension}"
+    with open(tempfile, "wb") as f:
+        f.write(audio_file.file.read())
+    text = extract_audio(tempfile)
+
+    if os.path.exists(tempfile):
+        os.remove(tempfile)
+        
+    return text
