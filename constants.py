1e1f5460184de69fea6c0bb013d178c08c7338d7
Arun-Gopalakrishnan <gavk.1986@gmail.com>
2024-07-12 19:35:28 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,4 @@
+#EXTRACT_URI = 'https://ec-task-automation.onrender.com/extract_task_entity_from_audio'
+#EXTRACT_URI = 'https://ec-task-automation.onrender.com/extract_task_entity_from_text'
+EXTRACT_FROM_AUDIO_URI = 'http://127.0.0.1:8000/extract_task_entity_from_audio'
+EXTRACT_FROM_TEXT_URI = 'http://127.0.0.1:8000/extract_task_entity_from_text'
