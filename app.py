814e24bda41b6dba523400ef1325cf2ba65b5cb9
Arun-Gopalakrishnan <gavk.1986@gmail.com>
2024-07-12 19:34:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+from pydantic import BaseModel
 import streamlit as st
-from streamlit_utils import get_api_response
+from streamlit_utils import extract_from_audio, extract_from_text
 from logging.config import dictConfig
 from logconfig import log_config
 import logging
+import requests
+from typing import TypeVar, Type
+from utils import APIResponse
 
+dictConfig(log_config)
 logger = logging.getLogger("exTaskAutomation-logger")
 
 st.title("EC - Task Automation")
@@ -12,23 +17,52 @@ supported_files = ['mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav', 'webm']
 
 uploaded_file = st.file_uploader("Upload your file", type=supported_files)
 use_whisper = st.checkbox("Use Whisper", value=True, disabled=True)
+#use_whisper = st.radio("Use Whisper", value=True, disabled=True)
+task = st.text_area("Task:", placeholder="Enter your text here!", height=200, max_chars=250)
 
-if uploaded_file is not None:
-    st.write("Filename:", uploaded_file.name)
-    st.write("Filetype:", uploaded_file.type)
+T = TypeVar('T')
 
-    if st.button('Extract Entities'):
+def showErrorInfo(err: T):
+    st.error(f"Failed to extract entity from audio file!")
+    st.write('Exception:')
+    if type(err) == Exception:
+        exp = Exception(err)
+        st.write(exp)
+    else:
+        msg = str(err)
+        st.write(msg)
+
+def showOutput(res: APIResponse):
+    if res.success:                            
+        st.success(f"Entity extracted Successfully!")
+        st.write('Task Details:')
+        st.write(res.resultJSON)      
+    else:
+        showErrorInfo(res.resultText)
+        st.write(f"status code: {res.statusCode}")
+        st.write(f"content: {res.content}")
+
+if st.button('Extract Entities'):
+    if uploaded_file is not None:
+        st.write("Filename:", uploaded_file.name)
+        st.write("Filetype:", uploaded_file.type)
+        
         with st.spinner(f"Parsing the file - {uploaded_file.name}..."):
             if uploaded_file is not None:
-                logger.info("Test")
-                res = get_api_response(
-                        uploaded_file
-                    )
-                if res.startswith("Traceback"):
-                    st.write('Exception:')
-                    st.error(res)
-                else:                   
-                    st.success(f"Entities extracted Successfully!")
-                    st.write('Task Details:')
-                    entities_json = res
-                    st.write(entities_json)
+                try:
+                    res = extract_from_audio(
+                                            uploaded_file
+                                        )                  
+                    showOutput(res)
+                except Exception as e:
+                    showErrorInfo(e)
+    elif task.count(task) > 0:
+        try:
+            res = extract_from_text(
+                                    task
+                                )
+            showOutput(res)
+        except Exception as e:
+            showErrorInfo(e)
+
+
